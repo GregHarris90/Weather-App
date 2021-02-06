dd1b0d1c00b433e670515280953ddd86053941fb
Greg Harris <ghart.productions@gmail.com>
2021-02-05 19:23:26 -0800

    commented out ajax response sections

--- script.js
+++ script.js
@@ -39,29 +39,39 @@ function getApi(event) {
     var API_key = "0b351b99223ceb38cc5d35d442babfe4"
     var requestURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&units=imperial&appid=" + API_key + "";
 
-    // ajax call for Main URL
+    // Main ajax call for Main URL
     $.ajax({
         url: requestURL,
         method: "GET",
+    // Run city response function
     }).then(function (response) {
-
+        // API URL variable for weather icon
         var iconURL = " https://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png";
-
+        // Using response data to display to webpage
+        // Add city name + current day (from moment.js)
         $(".cityName").text(response.name) + $(".currentDay").text(currentDay);
+        // Add temperature
         $(".temp").text(response.main.temp);
+        // Add humidity
         $(".humidity").text(response.main.humidity);
+        // Add wind speed
         $(".windspeed").text(response.wind.speed);
+        // Add icon to image src
         $(".icon").attr("src", iconURL);
 
+        // API URL variable for UV Index (lat+lon)
         var UVindex = "https://api.openweathermap.org/data/2.5/uvi?lat=" + response.coord.lat + "&lon=" + response.coord.lon + "&appid=" + API_key + "";
 
+        // Second ajax call for UV Index
         $.ajax({
             url: UVindex,
             method: "GET",
+        // Run UVIndex response function
         }).then(function (UVresponse) {
+            // Add UVIndex value display to webpage
             $(".UVindex").text(UVresponse.value);
         
-
+        // console logs for checking information
         console.log("UV Index Response:")
         console.log(UVindex);
         console.log("Icon Response:")
