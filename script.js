94a01bc9e9ede659b00f9d92b4ac8a57123fe069
Greg Harris <ghart.productions@gmail.com>
2021-02-05 18:37:55 -0800

    create weather icon api call and display to page

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 // TODOS:
-// Build fetch method to retrieve (City name/Weather) data
-// Append list of searched cities on sidebar
-// Display data to Main section ( City Name, Temperature, Humidity, Wind Speed, UV Index)
+// Display data to Main section UV Index)
 // Append list of 5 day forecast at bottom of screen
 // Create condition for UV Index box color
 
@@ -22,13 +20,17 @@ console.log("Current Day:",currentDay);
 function getApi(event) {
     event.preventDefault();
 
+    // 
     var cityInput = document.querySelector("#search-input").value;
     console.log (cityInput);
 
+    // city search history
     var searchHistory = $("#past-Search");
     var pastSearch = document.createElement("li");
     pastSearch.textContent = cityInput;
+    
     searchHistory.append(pastSearch)
+
 
     console.log(searchHistory);
 
@@ -36,36 +38,41 @@ function getApi(event) {
     // URL and API key variables
     var API_key = "0b351b99223ceb38cc5d35d442babfe4"
     var requestURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&units=imperial&appid=" + API_key + "";
-    
+
     $.ajax({
         url: requestURL,
         method: "GET",
     }).then(function (response) {
+
+        var iconURL = " https://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png";
+
         $(".cityName").text(response.name) + $(".currentDay").text(currentDay);
         $(".temp").text(response.main.temp);
         $(".humidity").text(response.main.humidity);
         $(".windspeed").text(response.wind.speed);
+        $(".icon").attr("src", iconURL);
         $(".UVindex").text();
 
+        
 
-
+        console.log(iconURL);
         console.log("Ajax Response");
         console.log(response);
     })
 
 
-    var forecast = document.querySelector("ul");
-    var requestForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityInput + "&appid=" + API_key + "";
+    // var forecast = document.querySelector("ul");
+    // var requestForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityInput + "&appid=" + API_key + "";
 
-    $.ajax({
-        url: requestForecast,
-        method: "GET",
-    }).then(function (response) {
-        $(".forecast").
+    // $.ajax({
+    //     url: requestForecast,
+    //     method: "GET",
+    // }).then(function (response) {
+    //     $(".forecast").
 
-        console.log("Forecast Response");
-        console.log(response);
-    })
+    //     console.log("Forecast Response");
+    //     console.log(response);
+    // })
 
   
 }
